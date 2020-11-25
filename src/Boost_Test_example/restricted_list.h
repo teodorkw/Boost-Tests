1e7e270f4c332ec38d29beb65bc5541e25dde502
teodorkw <balboa111111@interia.pl>
2020-11-25 18:29:00 +0100

    tests of adding elements and access to front of list

--- src/Boost_Test_example/restricted_list.h
+++ src/Boost_Test_example/restricted_list.h
@@ -6,6 +6,28 @@
 template <typename T, typename = typename std::enable_if<std::is_arithmetic<T>::value, T>::type>
 class RestrictedList
 {
+	class ProxyRestrictedList
+	{
+		T *val;
+		bool isRestricted;
+		T min, max;
+	public:
+		ProxyRestrictedList(T &val, bool isRestricted, T min, T max) : 
+			val(&val), isRestricted(isRestricted), min(min), max(max)
+		{}
+		T& operator=(T v)
+		{
+			if (isRestricted && (v < min || v > max))
+				throw std::exception("Invalid range");
+			*val = v;
+			return *val;
+		}
+		bool operator==(T v)
+		{
+			return *val == v;
+		}
+	};
+
 	std::list<T> l;
 	T min, max;
 	bool isRestricted;
@@ -16,5 +38,28 @@ public:
 	{
 		if (min > max)
 			throw std::exception("min value must be greater (or equal) than max");
+	}
+	void push_back(T elem)
+	{
+		if(isRestricted && (elem < min || elem > max))
+			throw std::exception("inserted element not in specified range");
+		l.push_back(elem);
+	}
+	void pop_back(T elem)
+	{
+		if (isRestricted && (elem < min || elem > max))
+			throw std::exception("inserted element not in specified range");
+		l.pop_back(elem);
+	}
+	ProxyRestrictedList front()
+	{
+		try 
+		{
+			return ProxyRestrictedList(l.front(), isRestricted, min, max);
+		}
+		catch (...)
+		{
+			throw;
+		}
 	}
 };
--- src/Tests/main.cpp
+++ src/Tests/main.cpp
@@ -15,3 +15,19 @@ BOOST_AUTO_TEST_CASE(constructors_test)
 	BOOST_CHECK_THROW(RestrictedList<int>(123, 4), std::exception);
 	BOOST_CHECK_THROW(RestrictedList<float>(4.f, 1.f), std::exception);
 }
+
+BOOST_AUTO_TEST_CASE(inserting_test)
+{
+	RestrictedList<int> list1(10, 50);
+	BOOST_CHECK_NO_THROW(list1.push_back(12));
+	BOOST_CHECK_THROW(list1.push_back(52), std::exception);
+
+	list1.push_back(28);
+	list1.push_back(23);
+	list1.push_back(32);
+
+	BOOST_CHECK_NO_THROW(list1.front() = 15);
+	BOOST_CHECK(list1.front() == 15);
+	BOOST_CHECK_THROW(list1.front() = 51, std::exception);
+	BOOST_CHECK(list1.front() == 15);
+}
